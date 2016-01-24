3299ba8740d0a3117d80220629b22567541eb0f4
Grzesiek <grzesiek.kyc@gmail.com>
2016-01-24 19:43:21 +0100

    task ch2 -17 added

---
+++ JavaScript Functions(29)/17.js
@@ -0,0 +1,8 @@
+// 17. Write a JavaScript function to  get the number of occurrences of each letter in specified string.
+
+function lettersCounter(str) {  
+	var letters = {};  
+	str1.replace(/\S/g, function(letter){uchars[letter] = (isNaN(uchars[lletter]) ? 1 : uchars[letter] + 1);});  
+
+	return letters;  
+} 
